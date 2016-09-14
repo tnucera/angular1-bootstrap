605a6ba06902a3958ab32e60c396bf438cbbc593
tnucera <thibault.nucera@gmail.com>
2016-09-14 17:33:05 +0200

    injector

--- injector.js
+++ injector.js
@@ -6,27 +6,45 @@
     var colors = require('colors');
     var conf = require('./conf.js');
 
-    function symlink(srcpath, dstpath) {
-        fse.ensureSymlink(srcpath, dstpath, function (err) {
+    function symlink(srcPath, dstPath) {
+        fs.symlink(srcPath, dstPath, function (err) {
             if (err) return console.error(err);
 
-            var type = 'file';
-            var stats = fs.lstatSync(srcpath);
-            if (stats.isDirectory()) {
-                type = 'dir';
-            }
-            console.log("Symlink done ".magenta + dstpath + " -> " + srcpath + " " + type.yellow);
+            var srcStat = fs.lstatSync(srcPath);
+            var srcType = (srcStat.isDirectory()) ? 'dir' : 'file';
+            console.log("Symlink created ".magenta + dstPath + " -> " + srcPath + " " + srcType.yellow);
         });
     }
 
+    function symlinkForce(srcPath, dstPath) {
+        try {
+            var dstStat = fs.lstatSync(dstPath);
+            // If destination file or dir exists and isn't a symlink
+            if (!dstStat.isSymbolicLink()) {
+                var dstType = (dstStat.isDirectory()) ? 'Dir' : 'File';
+                // Remove destination file or dir
+                fse.remove(dstPath, function (err) {
+                    if (err) return console.error(err);
+
+                    console.log((dstType + " removed ").magenta + dstPath);
+                    // Create symlink
+                    symlink(srcPath, dstPath);
+                });
+            }
+        } catch (e) {
+            // Symlink doesn't exist, so we create it
+            symlink(srcPath, dstPath);
+        }
+    }
+
     // Symlink node_modules
-    symlink(__dirname + '/node_modules', __dirname + '/dist/node_modules');
+    symlinkForce(__dirname + '/node_modules', __dirname + '/dist/node_modules');
     // Symlink src index
     conf.js.index.forEach(function (element, index, array) {
-        symlink(__dirname + '/' + element, __dirname + '/dist/' + element.substring(4));
+        symlinkForce(__dirname + '/' + element, __dirname + '/dist/' + element.substring(4));
     });
     // Symlink src app
-    symlink(__dirname + '/src/app', __dirname + '/dist/app');
+    symlinkForce(__dirname + '/src/app', __dirname + '/dist/app');
 
     // Inject in fileOut
     var fileOut = 'dist/index.html';
